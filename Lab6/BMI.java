48afd667e4872b937066472c86757a7cda0c253a
DaoRungphailin <88391247+DaoRungphailin@users.noreply.github.com>
2022-02-14 17:36:12 +0700

    1st

--- Lab6/BMI.java
+++ Lab6/BMI.java
@@ -1,54 +1,38 @@
 import java.util.*;
 public class BMI{
-    
-    String name;
+    //Attribute
+    private String name;
     private int age;
     private double weight;
-    private double height;
+    private double feet;
+    private double inches;
+    private double bmi;
+
     Scanner input = new Scanner(System.in);
- // Constructor
-    public BMI(String name, int age, double weight, double height) {
+    
+    // Constructor
+    public BMI(String name, int age, double weight,double feet, double inches) {
         this.name = name;
         this.age = age;
         this.weight = weight;
-        this.height = height;
+        this.feet = feet;
+        this.inches = inches;
     }
 
-    //Method
-    public int getName(){
-        return this.name;
+    public double getBMI(){
+        bmi = (weight*0.45359237)/((12 * feet + inches)*0.0254*(12 * feet + inches)*0.0254);
+        return bmi;
     }
 
-    public void setName(String name) {
-        String[] this.name = new String[5];
+    public String getInterpretation(){
+        if(bmi < 18.5)
+            return "Underweight";
+        else if(bmi >= 18.5 && bmi < 25.0)
+            return "Normal";
+        else if(bmi >= 25.0 && bmi < 30.0)
+            return "Overweight";
+        else if(bmi >= 30.0)
+            return "Obese";
+        return "ERROR";
     }
-
-    public int getAge(){
-        return this.age;
-    }
-
-    public void setAge(int age) {
-        this.age = input.nextInt();
-    }
-
-    public double getWeight(){
-        return weight;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = input.nextDouble();
-    }
-
-     public double getHeight(){
-        return height;
-    }
-
-    public void setHeight(double height) {
-        this.height = input.nextDouble();
-    } 
-
-    public void setUnit(double weight, double height){
-        this.weight = 0.45359237*weight;
-        this.height = 0.0254*height;
-    }
-}
+}   
--- Lab6/Pro1_64011258.java
+++ Lab6/Pro1_64011258.java
@@ -1,6 +1,26 @@
+import java.util.*;
 public class Pro1_64011258 {
 
     public static void main(String[] args) {
-       
+
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter Name: ");  
+        String name =  input.nextLine();
+
+        System.out.print("Enter Age: ");  
+        int age = input.nextInt();
+
+        System.out.print("Enter Weight(pounds): ");  
+        double weight = input.nextDouble();
+
+        System.out.print("Enter Height(feet): ");  
+        double feet = input.nextDouble();
+         System.out.print("Enter Height(inches): ");  
+        double inches = input.nextDouble();
+
+        BMI bmi = new BMI(name, age, weight, feet, inches);
+        System.out.println("BMI :" + bmi.getBMI());
+        System.out.println("Interpretation :" + bmi.getInterpretation());
     }
 }
