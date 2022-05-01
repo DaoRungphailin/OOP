bd53b4f276e89dd32b964059811a87660243e5aa
DaoRungphailin <88391247+DaoRungphailin@users.noreply.github.com>
2022-05-01 23:28:26 +0700

    Add Question

--- Lab1/C_PlusNum.java
+++ Lab1/C_PlusNum.java
@@ -1,3 +1,13 @@
+/*Program# 3(Exercise 2.6 p.70Sum the digits in an integer) 
+Write a program that reads an integer between 0 and1000 and adds all the digits in the integer. 
+For example, if an integer is 932, thesum of all its digits is 14.
+
+Hint:Use the % operator to extract digits anduse the / operator to remove theextracted digit. 
+For instance, 932 % 10 = 2 and 932 / 10 = 93. 
+
+Here is a sample run:
+Enter a number between 0 and 1000: 999
+<Enter>The sum of the digits is 27*/
 import java.util.Scanner;
 class C_PlusNum{
     public static void main(String[] args)
--- Lab1/D_BMI.java
+++ Lab1/D_BMI.java
@@ -1,3 +1,13 @@
+/*Program# 4(Exercise 2.14 p.72Health application: computing BMI) 
+Body Mass Index (BMI) is a measure ofhealth on weight. 
+It can be calculated by taking your weight in kilograms anddividing by the square of your height in meters. 
+Write a program that prompts theuser to enter a weight in pounds and height in inches and displays the BMI. 
+Notethat one pound is 0.45359237 kilograms,and one inch is 0.0254 meters. 
+
+Here isa sample run:
+Enter weight in pounds: 95.5
+<Enter>Enter height in inches: 50
+<Enter>BMI is 26.8573*/
 import java.util.Scanner;
 class D_BMI{
     public static void main(String[] args)
